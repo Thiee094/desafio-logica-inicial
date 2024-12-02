219d61fe118af97afc405e576dddf74e4845a40c
Thiee094 <cursonline203@gmail.com>
2024-12-02 15:27:14 -0300

    Desafio Dio XP Herói

---
+++ index.js
@@ -0,0 +1,60 @@
+//Desafio Classificador de nível de Herói.
+//o que deve ser utilizado
+
+
+// - Variaveis ( Nome do Heroi and a quantidade de XP)
+// -Operador
+// Laços de repetição
+// Estrutura de decisões
+
+//Objetivo do Projeto
+
+// Crie uma variavel para armanezar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+// Se XP for menor do que 1.00 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata 
+// Se XP for entre 6.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina Diamante
+// Se XP for entre 8.001 e 9000 = Ascendente
+// Se XP for entre 9.001 e 10.000 = Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+
+//Saída
+
+// ao final deve se exibir uma mensagem: O Herói de nome {nome} está no nivel de {nivel}.
+
+// criado a Lista de herois.
+let heroi = ['Aço','Capitão Valor', 'Fenix Dourada ',' Arcanjo Rubro', 'Pixel Defensor','TechVanguard','Guardião das Chamas','Eclipse Arcano']
+
+//variavel para armazenar o nivel XP.
+let categoria = ''
+
+// NivelXP informado para categorizar.
+let nivelXP = 10003
+
+//criando a condição.
+
+if (nivelXP <= 1000) {
+    categoria = 'Ferro'}
+
+ else if (nivelXP <= 2001) {
+    categoria = 'Bronze' }
+
+ else if (nivelXP <= 6001) {
+    categoria = 'Prata' }
+
+ else if (nivelXP <= 7001) {
+    categoria = 'Platina Diamante'}
+
+ else if (nivelXP <= 8001) {
+    categoria = 'Ascendente' }
+
+ else if (nivelXP <= 9001) {
+    categoria = 'Imortal' }
+
+ else if (nivelXP >= 10001) {
+    categoria = 'Radiante' }
+
+//Saída
+console.log ('O Herói ' + heroi[6] + ' está no nivel ' + categoria)
